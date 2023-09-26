c4fbe54891a67a55d4f9c00fe980d65373df5bec
gaspar <gasparmunoz@hotmail.com>
2023-09-26 15:10:36 -0300

    correcion texto about2A

--- src/components/ITCreative/About2A.jsx
+++ src/components/ITCreative/About2A.jsx
@@ -18,8 +18,8 @@ const About2A = () => {
               </div>
               <p className="color-666 wow fadeInUp">
                 {" "}
-                AVIVATE MÁS una experiencia vivencial, para píblico general. Es
-                un programa de educación convencional combinando, +
+                AVIVATE MÁS una experiencia vivencial, para público general. Es
+                un programa de educación no convencional combinando, +
                 actualización tecnológica, arte, comunicación, creatividad,
                 estrategia persuasión digital con alto grado de compromiso en
                 toda la matriz tecnológica que nos toca vivir como individuos de
